ccf056a72413e18e10a2ecaf4a7d6f602051735d
Ps1h0 <platonov.liceum@mail.ru>
2022-10-05 11:32:40 +0300

    Доработана логика, добавлена аннотация Getter

--- Common/src/main/java/com/example/common/FileInfo.java
+++ Common/src/main/java/com/example/common/FileInfo.java
@@ -1,77 +1,44 @@
 package com.example.common;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Arrays;
 import java.util.Objects;
 
-//Класс с информацией о файле
+/**
+ * Класс с информацией о файле
+ */
+@Getter
 public class FileInfo extends AbstractMessage {
-    //Перечисление типов файлов (файл или директория)
-    public enum FileType{
-        FILE("File"), DIRECTORY("Dir");
 
+    @AllArgsConstructor
+    @Getter
+    public enum FileType {
+        FILE("File"),
+        DIRECTORY("Dir");
         private final String name;
-
-        FileType(String name) {
-            this.name = name;
-        }
-
-        public String getName() {
-            return name;
-        }
     }
 
-    private String filename;
-    private FileType type;
-    private long size;
+    private final String filename;
+    private final FileType type;
+    private final long size;
     private byte[] fileContent;
 
-
-    public String getFilename() {
-        return filename;
-    }
-
-    public void setFilename(String filename) {
-        this.filename = filename;
-    }
-
-    public byte[] getFileContent() {
-        return fileContent;
-    }
-
-    public void setFileContent(byte[] fileContent) {
-        this.fileContent = fileContent;
-    }
-
-    public FileType getType() {
-        return type;
-    }
-
-    public void setType(FileType type) {
-        this.type = type;
-    }
-
-    public long getSize() {
-        return size;
-    }
-
-    public void setSize(long size) {
-        this.size = size;
-    }
-
-    public FileInfo() {
-    }
-
-    public FileInfo(Path path){
+    public FileInfo(Path path) {
         try {
             this.filename = path.getFileName().toString();
-            this.size = Files.size(path);
-            this.type = Files.isDirectory(path) ? FileType.DIRECTORY : FileType.FILE;
-            if (this.type == FileType.DIRECTORY) {
+            if (Files.isDirectory(path)) {
+                this.type = FileType.DIRECTORY;
                 this.size = -1L;
-            } else this.fileContent = Files.readAllBytes(path);
+            } else {
+                this.type = FileType.FILE;
+                this.size = Files.size(path);
+                this.fileContent = Files.readAllBytes(path);
+            }
         } catch (IOException e) {
             throw new RuntimeException("Некорректный файл");
         }
